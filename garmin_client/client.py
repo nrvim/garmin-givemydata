996ec26ead016c61348cf3cfa8f4695838dd23a9
Alex <nrvim@users.noreply.github.com>
2026-04-06 14:23:07 +0200

    fix(lint): sort imports, bump version to 0.1.9

--- garmin_client/client.py
+++ garmin_client/client.py
@@ -20,9 +20,9 @@ from datetime import date, datetime, timedelta
 from pathlib import Path
 from typing import Optional
 
+from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from seleniumbase import Driver
